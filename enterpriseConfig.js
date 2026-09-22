a13f4d068c6c2a32a82e47d112c0ac1fbddf0f47
abooraamoraa-code <abooraamoraa@gmail.com>
2026-09-22 16:07:03 +0300

    Create enterpriseConfig.js

---
+++ enterpriseConfig.js
@@ -0,0 +1,7 @@
+// إعدادات التوثيق الرسمية للمشروع
+module.exports = {
+    enterpriseName: "إدارة وتطوير أبو العز العمري",
+    version: "3.0.0-Ultimate",
+    securityLevel: "Maximum-Military-Grade",
+    author: "أبو العز العمري"
+};
