dbefadff28e2fe6150c61097c9c30e11e4ad5506
abooraamoraa-code <abooraamoraa@gmail.com>
2026-09-22 16:03:50 +0300

    Create analyticsEngine.js

---
+++ analyticsEngine.js
@@ -0,0 +1,6 @@
+// تتبع عدد المشاهدين المتواجدين ونقرات الإعلانات
+function trackVisitorActivity(req, res, next) {
+    // تسجيل إحصائيات الأداء في خلفية النظام
+    next();
+}
+module.exports = { trackVisitorActivity };
