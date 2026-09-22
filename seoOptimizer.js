442e8283f20d89b9d78baf454730c96d1d280c35
abooraamoraa-code <abooraamoraa@gmail.com>
2026-09-22 16:04:33 +0300

    Create seoOptimizer.js

---
+++ seoOptimizer.js
@@ -0,0 +1,5 @@
+// حقق تصدراً كاملاً في محركات البحث لجلب آلاف الزوار مجاناً لموقع البث
+function getSeoMetaTags() {
+    return `<meta name="description" content="موقع البث المباشر الأول للمباريات - إدارة وتطوير أبو العز العمري">`;
+}
+module.exports = { getSeoMetaTags };
