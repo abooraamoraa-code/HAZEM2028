e976095efea1725b0f52b72a2c441198f4675c27
abooraamoraa-code <abooraamoraa@gmail.com>
2026-09-22 16:03:27 +0300

    Create live-chat.js

---
+++ live-chat.js
@@ -0,0 +1,5 @@
+// نظام إدارة التعليقات والدردشة الحية للزوار أثناء مشاهدة المباريات
+function handleLiveChatConnection(socket) {
+    console.log('💬 زائر جديد انضم لدردشة البث المباشر - إدارة وتطوير أبو العز العمري');
+}
+module.exports = { handleLiveChatConnection };
